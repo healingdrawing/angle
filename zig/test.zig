603393b5f098c3f494cf9c178fe061c50c550f8c
healingdrawing <healingdrawing@gmail.com>
2026-08-17 21:33:07 +0300

    fix/hardcode tests zig f32 precision epsilon from 1e-6 to 1e-5.
    Otherwise custom test fails with values about 1_000_000 from zig side. Typescript looks like f64 by default for numbers.
    
    zig f64 precision works with 1e-6.

--- zig/test.zig
+++ zig/test.zig
@@ -7,7 +7,7 @@ const test_from = @import("tests/test_from.zig").test_from;
 pub fn main(init: std.process.Init) !void {
     dp.init_from_env_map(init.environ_map);
 
-    const epsilon: f32 = 1e-6;
+    const epsilon: f32 = 1e-5; //warning f32 with 1e-6 fails with values about 1000000
 
     const results = [_]report.MethodResult{
         try test_from(epsilon),
